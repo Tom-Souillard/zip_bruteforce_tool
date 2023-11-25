54f9a196bac62f80c53f9bb6f42aee3d457a6874
Tom Souillard <tom.souillard@gmail.com>
2023-11-25 20:12:42 +0100

    [chore] Add constants file for global definitions
    - Define default constants for brute force settings
    - Define paths and file names

---
+++ zip_bruteforce_tool/constants.py
@@ -0,0 +1,8 @@
+# Constants for bruteforce settings
+DEFAULT_MIN_LENGTH = 1
+DEFAULT_MAX_LENGTH = 8
+DEFAULT_CHARSET = 'aA1'
+
+# Paths and file names
+HASH_FILE = 'zip_hash.txt'
+TEST_ZIP_FILE = 'test.zip'
